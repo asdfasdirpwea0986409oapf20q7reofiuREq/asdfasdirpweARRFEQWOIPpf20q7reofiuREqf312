8750842d2ca08835be20f0ccdf1e130b9875cb02
et-sollertis-animi <hiaarushgupta@gmail.com>
2020-12-21 18:24:14 -0800

    added urls for all database elements and 406 message

--- api/TODO.md
+++ api/TODO.md
@@ -1,5 +1,8 @@
 # todo
-- [ ] create endpoints for question, answer, and user; allow methods used to run the needed code
+- [x] create endpoints for question, answer, user, and tackboard; allow methods used to run the needed code
 - [ ] make a tool to export and import data into database as json
 - [ ] create unit tests for all tables
 - [ ] add event handling for all database functions, return error like api
+- [ ] making a toolbox api for the api so that we don't lose space on the REPL but stil have an advanced api
+- [ ] make another database for low-prority items (points)
+- [ ] make unit tests for api
--- api/api.py
+++ api/api.py
@@ -21,6 +21,56 @@ def homepage():
     if request.method == "GET":
         return jsonify({"message" : "welcome to the Tackboard api!"}), 200
 
+@app.route("/users", methods = ["POST", "GET", "PATCH", "DELETE"])
+def user():
+    connection = database.users.connect()
+    try:
+        values = json.loads(request.get_json())
+    except TypeError:
+        pass # means that request is GET or faulty
+    try:
+        if request.method == "POST":
+            database.users.create(connection, id = values["id"], created = values["created"], username = values["username"], name = values["name"], email = values["email"], asked = values["asked"], answered = values["answered"])
+            return jsonify({"message" : "success"}), 201
+        elif request.method == "GET":
+            data = database.users.retreive(connection, int(request.args.get("id")))
+            return jsonify(data), 200
+        elif request.method == "PATCH":
+            database.users.update(connection, values["id"], values["column"], values["new"])
+            return jsonify({"message" : "success"}), 200
+        elif request.method == "DELETE":
+            database.users.delete(connection, values["id"])
+            return jsonify({"message" : "success"}), 200
+    except KeyError: # user did not pass in necessary data 
+        abort(406)
+    except TypeError: # user did not pass in id in url for retrieve method
+        abort(406)
+
+@app.route("/tackboards", methods = ["POST", "GET", "PATCH", "DELETE"])
+def tackboard():
+    connection = database.tackboards.connect()
+    try:
+        values = json.loads(request.get_json())
+    except TypeError:
+        pass # means that request is GET or faulty
+    try:
+        if request.method == "POST":
+            database.tackboards.create(connection, id = values["id"], name = values["name"], subject = values["subject"], description = values["description"], public = values["public"], ownerID = values["ownerID"], created = values["created"], memberIDs = values["memberIDs"], questionIDs = values["questionIDs"], answersIDs = values["answersIDs"])
+            return jsonify({"message" : "success"}), 201
+        elif request.method == "GET":
+            data = database.tackboards.retreive(connection, int(request.args.get("id")))
+            return jsonify(data), 200
+        elif request.method == "PATCH":
+            database.tackboards.update(connection, values["id"], values["column"], values["new"])
+            return jsonify({"message" : "success"}), 200
+        elif request.method == "DELETE":
+            database.tackboards.delete(connection, values["id"])
+            return jsonify({"message" : "success"}), 200
+    except KeyError: # user did not pass in necessary data 
+        abort(406)
+    except TypeError: # user did not pass in id in url for retrieve method
+        abort(406)
+
 @app.route("/questions", methods = ["POST", "GET", "PATCH", "DELETE"])
 def question():
     connection = database.questions.connect()
@@ -41,7 +91,34 @@ def question():
         elif request.method == "DELETE":
             database.questions.delete(connection, values["id"])
             return jsonify({"message" : "success"}), 200
-    except KeyError:
+    except KeyError: # user did not pass in necessary data 
+        abort(406)
+    except TypeError: # user did not pass in id in url for retrieve method
+        abort(406)
+
+@app.route("/answers", methods = ["POST", "GET", "PATCH", "DELETE"])
+def answer():
+    connection = database.answers.connect()
+    try:
+        values = json.loads(request.get_json())
+    except TypeError:
+        pass # means that request is GET or faulty
+    try:
+        if request.method == "POST":
+            database.answers.create(connection, id = values["id"], questionID = values["questionID"], authorID = values["authorID"], upvotes = values["upvotes"], downvotes = values["downvotes"], created = values["created"], answer = values["answer"])
+            return jsonify({"message" : "success"}), 201
+        elif request.method == "GET":
+            data = database.answers.retreive(connection, int(request.args.get("id")))
+            return jsonify(data), 200
+        elif request.method == "PATCH":
+            database.answers.update(connection, values["id"], values["column"], values["new"])
+            return jsonify({"message" : "success"}), 200
+        elif request.method == "DELETE":
+            database.answers.delete(connection, values["id"])
+            return jsonify({"message" : "success"}), 200
+    except KeyError: # user did not pass in necessary data 
+        abort(406)
+    except TypeError: # user did not pass in id in url for retrieve method
         abort(406)
 
 if __name__ == "__main__":
